efb3d1a66ac3fec220c92d4e3b2e022247c709b1
eagleblade <jostein.rosenlund@gmail.com>
2018-11-25 15:30:08 +0100

    Added Check_group

--- check_user/users.sh
+++ check_user/users.sh
@@ -11,3 +11,16 @@ check_user() {
       echo "user: $1 does not exist"
     fi
 }
+
+#checks if an group exsist or not, in /etc/group
+# Usage
+# check_group GROUP
+
+check_group() {
+  local GROUP=$(awk -v val=$1 -F ":" '$1==val{print $1}' /etc/group)
+    if [ "$GROUP" > "0" ]; then
+         echo "group exists"
+    else
+         echo "group does not exist"
+    fi
+}
